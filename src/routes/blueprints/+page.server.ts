7ba7aabddaec848f77a5e5cb06c5a6df514b1939
meredori <durzan@gmail.com>
2025-11-18 15:45:41 +1000

    Fix blueprint redirect for base path

--- src/routes/blueprints/+page.server.ts
+++ src/routes/blueprints/+page.server.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = () => {
-  throw redirect(308, '/what-i-have');
+export const load: PageServerLoad = ({ url }) => {
+  const target = url.pathname.replace(/\/blueprints$/, '/what-i-have');
+
+  throw redirect(308, target);
 };
